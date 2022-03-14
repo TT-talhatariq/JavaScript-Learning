5bb98715b93a79a2568fe511cf475c13f5888f58
Talha Tariq <talhacoder01@gmail.com>
2022-03-14 19:11:25 +0500

    Challange One Done

---
+++ Fundamentals 1/challange#1/script.js
@@ -0,0 +1,32 @@
+// Mark Data
+let markWeight = 78;
+let markHeight = 1.69;
+
+// John Data
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+let markBMI;
+let johnBMI;
+markBMI = markWeight / (markHeight**2);
+johnBMI = johnWeight / (johnHeight**2);
+
+// Test Case 1
+console.log("Mark BMI: ", markBMI);
+console.log("John BMI: ", johnBMI);
+
+// Test Case 2
+markWeight = 95;
+markHeight = 1.88;
+johnWeight = 85;
+johnHeight = 1.76;
+
+
+markBMI = markWeight / (markHeight**2);
+johnBMI = johnWeight / (johnHeight**2);
+
+// Test Case 1
+console.log("Mark BMI: ", markBMI);
+console.log("John BMI: ", johnBMI);
+
+
