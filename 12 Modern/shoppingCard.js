dac19f60ef5de8f27f64c6a28e969adf4f6f9084
Talha Tariq <talhacoder01@gmail.com>
2022-06-17 10:59:19 +0500

    Git Fixed

--- 12 Modern/shoppingCard.js
+++
@@ -1,9 +0,0 @@
-console.log('Exporting');
-
-const cart = [];
-const shippingCost = 10;
-
-export const addToCard = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log('Added');
-};
